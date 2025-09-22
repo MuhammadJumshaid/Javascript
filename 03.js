325f987500e2d48fac0dd15e83c3d62ad34f7ad2
Muhammad Jumshaid <jumshaid.khan.1222@gmail.com>
2025-09-22 22:15:33 +0500

    20250922_03_Rules for naming variables

---
+++ 03.js
@@ -0,0 +1,30 @@
+//20250922
+
+// Rules for nameing variables 
+ 
+// You cannot start variable name with number 
+//
+//example 
+// 1value(invalid)
+// value1(valid)
+//
+//
+//
+//
+// You can only use underscore  _ or $ dollar sign symbol
+//
+// first_name (valid)
+  // _firstname (valid)
+//
+//
+//  first$name (valid)
+//  $firstname(valid)
+//
+//
+//  you cannot use spaces 
+//
+//  first name (invalid)
+// 
+//
+//  Conventions
+// Start with small letter and use camel case 
