064a8af068963bef24aa242b4e38a5aaffe6604e
Muhammad Jumshaid <jumshaid.khan.1222@gmail.com>
2025-09-23 20:57:36 +0500

    20250923-4-Let key word intro

---
+++ 04.js
@@ -0,0 +1,15 @@
+//20250923
+
+// Let keyword
+
+// you can decalre variable using let keyword instead of var keyword
+// var firstName = "Jumshaid";
+// var firstName = "Muhammad"
+// console.log(firstName)
+
+// this will not work in case of let 
+
+let firstName = "Jumshaid"
+ firstName = "Ali"
+
+ console.log(firstName)
