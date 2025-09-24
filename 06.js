d1afe646640250dd261e1e82302aca6c9e1c1ea4
Muhammad Jumshaid <jumshaid.khan.1222@gmail.com>
2025-09-24 20:46:17 +0500

    20250924_06_StringI ndexing

---
+++ 06.js
@@ -0,0 +1,26 @@
+// 20250924 
+
+// string indexing
+
+// each character of string has its unique string , index number starts form 0 
+
+//   M   u   h   a   m   m   a   d 
+//   0   1   2   3   4   5   6   7
+
+
+let firstName = "Jumshaid";
+console.log(firstName[3])  // s
+
+
+// length of string 
+// total Number of characters in a string are the length of string (spaces included)
+
+let lastName = "Khan sakhani" 
+
+console.log(lastName.length) // 12
+
+// last index  : length-1
+
+let test = "lksjsdfjsdoif sfjsdf i"
+console.log(test[test.length-1]) // i
+console.log(test[test.length-2]) // space 
