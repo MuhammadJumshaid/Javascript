b90bc75d9c4dd70803d2da95becd8c6e413f27ca
Muhammad Jumshaid <jumshaid.khan.1222@gmail.com>
2025-09-26 22:32:10 +0500

    20250926-10-Template String (literals)

---
+++ 10.js
@@ -0,0 +1,19 @@
+// 20250926 
+
+// Template String 
+
+// let myName = "Jumshaid";
+// let myAge  =  24 ;
+
+// let AboutMe = "My Name is " + myName + " My age is " + myAge
+
+// console.log(AboutMe); // My Name is Jumshaid My age is 24
+
+// Now using Template String 
+
+let myName = "Jumshaid";
+let myAge  =  24 ;
+
+let AboutMe = `My name is ${myName}, and my Age is ${myAge} , OK`
+
+console.log(AboutMe); // My Name is Jumshaid My age is 24
