f9d2c4ccb5db10c8bb06cd2041032684bbea87a3
Muhammad Jumshaid <jumshaid.khan.1222@gmail.com>
2025-09-26 22:23:40 +0500

    20250926-09-String Concatenation

---
+++ 09.js
@@ -0,0 +1,37 @@
+// 20250926
+
+// String Concatenation
+
+// let firstString = "Muhammad";
+// let secondString = "Jumshaid";
+// let fullString = firstString + secondString;
+
+// console.log(fullString); // MuhammadJumshaid
+
+
+// let firstString = "Muhammad";
+// let secondString = "Jumshaid";
+// let fullString = firstString +  " " + secondString;
+
+// console.log(fullString); // Muhammad Jumshaid
+
+
+// let firstString = "12";
+// let secondString = "10";
+// let fullString = firstString  + secondString;
+
+// console.log(fullString); // 1210
+
+
+let firstString = "12";
+let secondString = "10";
+let fullString = +firstString  + +secondString;
+
+console.log(fullString); // 22
+console.log(typeof fullString); // number
+
+
+
+
+
+
