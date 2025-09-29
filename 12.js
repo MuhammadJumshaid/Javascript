b72f60e466c9949349d22594312dfaaa116c9bf8
Muhammad Jumshaid <jumshaid.khan.1222@gmail.com>
2025-09-29 15:48:39 +0500

    20250929_12_1.Comparison operators and Booean , 2. == Vs === , 3.  != Vs !==

---
+++ 12.js
@@ -0,0 +1,74 @@
+// 20250929 
+
+// Booleans and Comparison Operator 
+
+// Booleans(True, False)
+// comparison operator (==, != , && , || , < , > , <= , >= )
+
+// let a = 6;
+// let b = 7;
+
+// console.log(a > b ); // false 
+// console.log(a==b);  // false
+// console.log(a != b); // true 
+// console.log(a<b); //true
+
+
+
+// == vs ===   
+
+// == compare only values but === compre values also its datatypes
+
+// let a = 6;
+// let b = 7;
+
+// console.log(a == b); // false
+
+
+
+// let a = 7;
+// let b = 7;
+
+// console.log(a == b); // true
+
+
+// let a = "7";
+// let b = 7;
+
+// console.log(a == b); // true
+
+
+// let a = "7;"
+// let b = 7;
+
+// console.log(a === b); // false
+
+
+// != vs !==
+
+
+
+// let a = "7"
+// let b = 7;
+
+// console.log(a != b); // false
+
+// let a = 6 ;
+// let b = 7;
+
+// console.log(a != b); // true
+
+
+
+// let a = 6 ;
+// let b ="7";
+
+// console.log(a !== b); // true
+
+
+let a = 6 ;
+let b ="6";
+
+console.log(a !== b); // true
+
+
