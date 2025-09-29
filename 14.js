256a6b16290bae1b85240fbf12fcc2410334702e
Muhammad Jumshaid <jumshaid.khan.1222@gmail.com>
2025-09-29 16:59:54 +0500

    20250929_14_truthy, falsy values

---
+++ 14.js
@@ -0,0 +1,77 @@
+// 20250929 
+
+// truthy and falsy values  
+
+// falsy values 
+// false
+// ""
+// undefined,
+// null ,
+// 0
+
+
+// if(""){
+//     console.log("print if statement");
+// }
+// else{
+//     console.log("print else statement");
+// }
+
+
+// if(undefined){
+//     console.log("print if statement");
+// }
+// else{
+//     console.log("print else statement");  
+// }
+
+
+
+
+// if(null){
+//     console.log("print if statement");
+// }
+// else{
+//     console.log("print else statement");  
+// }
+
+// if(0){
+//     console.log("print if statement");
+// }
+// else{
+//     console.log("print else statement");  
+// }
+
+// let firstName = false
+
+// if(firstName){
+//     console.log("print if statement");
+// }
+// else{
+//     console.log("print else statement");  
+// }
+
+
+// All the above statements result will be false as all are falsy values
+
+
+// let firstName = "Jumshaid"
+
+// if(firstName){
+//     console.log("print if statement");  // "Jumshaid"
+// }
+// else{
+//     console.log("print else statement");  
+// }
+
+// Except the above fasly values all other values are truthey
+// i.e "abc"  , 1, -1  etc
+
+ let age = -1
+
+if(age){
+    console.log("print if statement");  //print if statement
+}
+else{
+    console.log("print else statement");  
+}
