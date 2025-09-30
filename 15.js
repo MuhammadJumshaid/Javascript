5536f69b027639f57a89dc06e50d02e1c6e0e411
Muhammad Jumshaid <jumshaid.khan.1222@gmail.com>
2025-09-30 12:26:29 +0500

    20250930_15_Ternaray Operator ( Conditional Operator)

---
+++ 15.js
@@ -0,0 +1,23 @@
+// 20250930 
+
+// Ternary Operator 
+
+// let age = 5;
+let age = 14;
+// let drink ;
+
+// if(age >12 ){
+//     drink  = "Tea";
+// }
+// else {
+//     drink = "Milk"
+// }
+
+// console.log(drink);
+
+
+// Ternary Operator / Conditional Operator
+
+ let drink = age > 8 ? "tea" : "milk"
+
+ console.log(drink);
