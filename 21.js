90d291bc9384f02868d53729d085ef059225dbea
Muhammad Jumshaid <jumshaid.khan.1222@gmail.com>
2025-10-15 23:35:03 +0500

    20251015_21_While Loop example (sum of numbers)

---
+++ 21.js
@@ -0,0 +1,44 @@
+// 20251015
+
+// While loop example 
+// sum of first 10 natural number 
+
+// let number = 10;
+
+// let total = 0 ;
+
+// total = total + 1 ;
+// total = total + 2 ;
+// total = total + 3 ;
+// total = total + 4 ;
+// total = total + 5 ;
+// total = total + 6 ;
+// total = total + 7 ;
+// total = total + 8 ;
+// total = total + 9 ;
+// total = total + 10 ;
+// console.log(total);
+
+
+
+// usign while loop
+
+// let number = 10;
+
+// let total = 0 ;
+// let i = 0;
+// while(i<=10){
+// total = total + i ;
+// i++
+// }
+// console.log("total : ", total);
+
+
+
+// Math formula
+// Note >> this method is faster than the above methods as this is taking constant time while obove methods are taking linear time
+
+let n = 100
+ let total = (n * (n + 1)) / 2
+
+ console.log(total);
