8991620090252d988062f0200179879bfe865931
Muhammad Jumshaid <jumshaid.khan.1222@gmail.com>
2025-10-15 20:08:43 +0500

    20251015_20_While Loop

---
+++ 20.js
@@ -0,0 +1,57 @@
+// 20251015 
+
+// While Loop 
+
+// print 1 to 9 
+
+// console.log("1");
+// console.log("2");
+// console.log("3");
+// console.log("4");
+// console.log("5");
+// console.log("6");
+// console.log("7");
+// console.log("8");
+// console.log("9");
+
+// let i = 0; 
+
+// console.log(i);
+// i++  ;
+
+// console.log(i);
+// i++  ;
+
+// console.log(i);
+// i++  ;
+
+// console.log(i);
+// i++  ;
+
+// console.log(i);
+// i++  ;
+
+// console.log(i);
+// i++  ;
+
+// console.log(i);
+// i++  ;
+
+// console.log(i);
+// i++  ;
+
+// console.log(i);
+// i++  ;
+
+
+// Coding Principe (Dry(Do not Repeat Yourself))
+
+let i = 0 ;
+while(i<10){
+    console.log(i);
+    i++;
+}
+
+console.log(`current value of i is ${i}`);
+
+console.log(i);
