79a192efa03992809472b56016ea615e85764b3d
Muhammad Jumshaid <jumshaid.khan.1222@gmail.com>
2025-10-16 21:39:21 +0500

    20251016_24_Break and Continue Keyword

---
+++ 24.js
@@ -0,0 +1,32 @@
+// 20251016  
+
+// Break Keyword (Break key word stops the execution and comes outside of the code)
+
+
+for(let i = 0 ; i < 10 ; i++){
+    if(i == 4){
+        break;
+    }
+    console.log(i);
+}
+console.log("oustside of the loop");
+
+
+
+// Continue Keyword 
+//When code reach to continue , it comes again at loop codition 
+
+
+for(let i = 0 ; i <= 10 ; i ++){
+    if(i == 4){
+        continue;
+        
+    }
+    console.log(i);
+}
+
+console.log("outside the loop");
+
+
+
+
