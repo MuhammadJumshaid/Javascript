7e7420cb7262db99898a5a4c3bb77da34d8b51ec
Muhammad Jumshaid <jumshaid.khan.1222@gmail.com>
2025-10-16 22:57:56 +0500

    20251016_25_Do While Loop

---
+++ 25.js
@@ -0,0 +1,33 @@
+// 20251016 
+
+// Do While Loop 
+
+
+
+
+// While loop
+let a = 10 ;
+
+while(a<9){
+console.log(a);
+a++;
+}
+console.log("outside the loop");
+
+// if the first contion become false then while loop will not print anyting in the loop
+
+
+
+
+// Do while
+// Do while print the code first time althouh the condition is false
+
+ let  i = 10;
+
+do{
+    console.log(i);
+      i++ ;
+}while(i<=10)
+
+    console.log("value of i is : ", i);
+  
