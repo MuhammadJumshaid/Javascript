cf95719b0c2403b1dd53cc4af087a03ce9c6aac8
Muhammad Jumshaid <jumshaid.khan.1222@gmail.com>
2025-10-20 22:31:58 +0500

    20251020_32_While Loop in array

---
+++ 32.js
@@ -0,0 +1,12 @@
+// 20251020 
+
+// While Loop in array 
+
+let fruits = ["Banana", "Grapes", "Mango", "Cherry"]
+const fruits2 = [];
+let i = 0; 
+while(i<=fruits.length){
+ fruits2.push((fruits[i]))
+i++;
+}
+console.log("Fruits2",fruits2);
