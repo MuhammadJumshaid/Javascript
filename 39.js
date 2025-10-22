08a77f249e32b25e62ec4f217efd57813d20cd53
Muhammad Jumshaid <jumshaid.khan.1222@gmail.com>
2025-10-22 20:48:14 +0500

    20251022_39_Computed Properties

---
+++ 39.js
@@ -0,0 +1,54 @@
+// 20251022 
+
+// Computed Properties 
+
+// Task 
+
+// let key1 = "objKey1"
+// let key2 = "objKey2"
+// let value1 = "myValue1"
+// let value2 = "myValue2" 
+
+// Make the object like this 
+
+
+// const obj = {
+//     objKey1 : "myValue1",
+//     objKey2 : "myValue2"
+// }
+
+
+
+let key1 = "objKey1"
+let key2 = "objKey2"
+let value1 = "myValue1"
+let value2 = "myValue2" 
+
+
+// const obj = {
+//     key1 : value1,
+//     key2 : value2
+// }
+// console.log(obj); // {key1: 'myValue1', key2: 'myValue2'}
+
+
+
+
+const obj = {
+    [key1] : value1,
+    [key2] : value2
+}
+console.log(obj); // {objKey1: 'myValue1', objKey2: 'myValue2'}
+
+
+
+
+
+
+
+// const obj =  {}
+
+// obj[key1] = value1;
+// obj[key2]= value2
+
+// console.log(obj);
