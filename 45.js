cadca83f5aa541f1c86fe704f1a70518d77c4fc4
Muhammad Jumshaid <jumshaid.khan.1222@gmail.com>
2025-10-27 21:28:01 +0500

    20251027_45_Function Expression

---
+++ 45.js
@@ -0,0 +1,22 @@
+// 20251027 
+
+// Function Expression 
+
+// When we assign a function to a variable this is called function expression(Store funtion in varialbe),
+// The variable name will be treated as function name
+
+// Function Declartion
+// function sum(){
+//     return 2+4
+// }
+// console.log(sum()); // 6
+
+// Function Expression
+
+let sum = function(){
+    return 2+4
+}
+let result = sum();
+console.log(result) // 6
+
+// Practice yourself from the previous file functions
