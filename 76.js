4be57b5e3ece418f6250a797c1bbed5ecb8c3d12
Muhammad Jumshaid <jumshaid.khan.1222@gmail.com>
2025-11-22 20:09:13 +0500

    20251122_76_short method to write regular function inside objce

---
+++ 76.js
@@ -0,0 +1,39 @@
+// 20251122 
+
+
+//short method to write methods(function inside object)
+
+
+
+const user1 = {
+   
+    firsName :  'jumshaid',
+    age : 12,
+    about :function(){
+        console.log(this.firsName , this.age) //  Jumshaid 12
+    }
+}
+
+user1.about() // Jumshaid 12
+
+
+// with short
+
+
+
+const user2 = {
+   
+    firsName :  'Jimi',
+    age : 24,
+    about(){
+        console.log(this.firsName , this.age)
+    }
+    
+}
+
+user2.about() // Jimi 24
+
+
+
+// this short can't be applied on arrow function
+
