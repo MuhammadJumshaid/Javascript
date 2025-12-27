6247d7fdfa1679d4841f508edc25982d12d31198
Muhammad Jumshaid <jumshaid.khan.1222@gmail.com>
2025-12-27 23:48:02 +0500

    20251227_Part2_How Javascript works (Start)

---
+++ Part2-01.js
@@ -0,0 +1,15 @@
+// 20251227
+
+// js_part-2 
+
+
+// compilation 
+
+// why compilation
+
+// how javascript code exucutes
+// what is global execution context
+// what is loca execution context
+
+
+// Closures
