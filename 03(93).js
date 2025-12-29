2c5b53613857b862fe2da431334c3b1c024b0768
Muhammad Jumshaid <jumshaid.khan.1222@gmail.com>
2025-12-29 23:33:15 +0500

    20251229_94(03)

---
+++ 03(93).js
@@ -0,0 +1,10 @@
+// 20251229
+
+// Function expression
+
+console.log(myFun)   // undefined  
+var myFun =  function(){
+    console.log("Hi, Jimi, How are you")
+}
+
+console.log(myFun)   // whole function  >> 
