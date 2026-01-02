59632882e30d1e45c93cdc70f13179357e75d8fd
Muhammad Jumshaid <jumshaid.khan.1222@gmail.com>
2026-01-02 23:24:54 +0500

    20260102_99_Closures example

--- 98.js
+++ 98.js
@@ -3,7 +3,7 @@
 // Closueres 
 // A function remembers variables from its lexical scope, even after the outer function has finished execution. is called closures
 
-// when a function is retrun from a function this will get the variables from its local memeory (from where this function is returned) this is closure
+// when a function is retrun from a function this will get the variables from its local memeory (from where this function is returned(mens return with its lexical envirenment)) this is closure
 
 //  functions remember the scope in which they were CREATED, not where they are CALLED. This is called lexical scoping, and the memory that is kept alive is called a closure.
 
