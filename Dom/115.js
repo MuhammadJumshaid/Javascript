664d110dd30d6d61f9f282451bb0a1887c8b00cc
Muhammad Jumshaid <jumshaid.khan.1222@gmail.com>
2026-01-12 21:17:44 +0500

    20260113_115_inserAdjcentHTML(Same as document.CreateElement)

---
+++ Dom/115.js
@@ -0,0 +1,25 @@
+// 20260112 
+
+// inserAdjcentHTML(where html)
+// befrobegin (before)
+// afterbegin (prepend)
+// beforeend (append)
+// afterend  (after)
+
+
+// beforebegin
+// const todoList = document.querySelector(".todo-list")
+// console.log(todoList) // <ul class="todo-list">…</ul>
+// todoList.insertAdjacentHTML("beforebegin", "<li>Before ul</li>")
+
+// // afterbegin
+// const todoList = document.querySelector(".todo-list")
+// const todo = todoList.insertAdjacentHTML("afterbegin", "<li>fistTodo</li>")
+
+// beforeend
+// const todoList = document.querySelector(".todo-list")
+// const todo = todoList.insertAdjacentHTML("beforeend", "<li>lastTodo</li>")
+
+// // afterul
+// const todoList = document.querySelector(".todo-list")
+// const todo = todoList.insertAdjacentHTML("afterend", "afterul")
